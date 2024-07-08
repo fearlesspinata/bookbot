b8f1c3cff38741dc919462608fe8952bf1695701
fearlesspinata <vesnalay@gmail.com>
2024-07-07 20:08:55 -0700

    Added a final function to sort on values and print a report

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     text = get_book_text("books/frankenstein.txt")
+    word_count = count_words(text)
     char_map = count_characters(text)
-    for c, count in char_map.items():
-        print(c, count)
+    sorted_char_map = get_sorted(char_map)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    for item in sorted_char_map:
+        print(f"The '{item[0]}' character was found {item[1]} times")
+    print("--- End report ---")
 
 def get_book_text(book_path):
     with open(book_path) as b:
@@ -14,12 +19,19 @@ def count_words(text):
 def count_characters(text):
     char_map = {}
     for c in text:
-        if c.lower() in char_map:
-            char_map[c.lower()] += 1
-        else:
-            char_map[c.lower()] = 1
+        if c.lower().isalpha():
+            if c.lower() in char_map:
+                char_map[c.lower()] += 1
+            else:
+                char_map[c.lower()] = 1
 
     return char_map 
+
+def get_value(item):
+    return item[1]
+
+def get_sorted(dictionary):
+    return sorted(dictionary.items(), key=get_value, reverse=True)
 
 if __name__ == '__main__':
     main()
